2e996012e81267c3898ea1a813d75de49b91e373
Germain-L <germain.leignel@gmail.com>
2024-01-17 11:14:22 +0100

    logging

--- scrapper/main.py
+++ scrapper/main.py
@@ -2,18 +2,25 @@ import requests
 import csv
 import schedule
 import time
+import logging
 from datetime import datetime, timedelta
 
 # Configuration
 api_url = "https://data.opendatasoft.com/api/explore/v2.1/catalog/datasets/etat-du-trafic-en-temps-reel@rennes-metropole/records"
 end_time = datetime.now() + timedelta(days=1)  # Durée d'exécution de 24 heures
 
+# Setup logging
+logging.basicConfig(filename='traffic_data_log.log', level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+
 def fetch_and_save_traffic_data():
     if datetime.now() >= end_time:
+        logging.info("Scheduled job completed.")
         return schedule.CancelJob
 
-    response = requests.get(api_url)
-    if response.status_code == 200:
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+
         data = response.json().get('results', [])
         if data:
             timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
@@ -22,18 +29,21 @@ def fetch_and_save_traffic_data():
                 writer = csv.DictWriter(file, fieldnames=data[0].keys())
                 writer.writeheader()
                 writer.writerows(data)
-            print(f"Data saved in {filename} at {datetime.now()}")
+            logging.info(f"Data saved in {filename}")
         else:
-            print(f"No data to save at {datetime.now()}")
-    else:
-        print(f"Failed to fetch data at {datetime.now()}: {response.status_code}")
+            logging.warning("No data to save.")
+    except requests.HTTPError as http_err:
+        logging.error(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        logging.error(f"An error occurred: {err}")
 
-# Planifier l'exécution toutes les 5 minutes
+# Execute immediately and then schedule every 10 minutes
+fetch_and_save_traffic_data()
 schedule.every(10).minutes.do(fetch_and_save_traffic_data)
 
-# Exécuter le script pour la durée spécifiée
+# Run the script for the specified duration
 while datetime.now() < end_time:
     schedule.run_pending()
     time.sleep(1)
 
-print("Data collection completed.")
+logging.info("Data collection completed.")
